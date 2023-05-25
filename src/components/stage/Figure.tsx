5291b4cd575c69568fa0e462549e1165f84ab3e1
Mariusz Najwer <najwer23@gmail.com>
2023-05-25 22:45:45 +0200

    workaround for outline for firefox

--- src/components/stage/Figure.tsx
+++ src/components/stage/Figure.tsx
@@ -6,6 +6,7 @@ const Figure: FC<FigureProps> = ({dimensions}) => {
 		return (
 			<>
 				<Ellipse dimensions={dimensions} />
+				<BBEllipse dimensions={dimensions} />
 			</>
 		);
 	}
@@ -14,6 +15,7 @@ const Figure: FC<FigureProps> = ({dimensions}) => {
 		return (
 			<>
 				<Rect dimensions={dimensions} />
+				<BBRect dimensions={dimensions} />
 			</>
 		);
 	}
@@ -24,6 +26,95 @@ const Figure: FC<FigureProps> = ({dimensions}) => {
 
 export default Figure;
 
+// workaround for outline which doesnt work on firefox
+const BBRect: FC<FigureProps> = ({dimensions}) => {
+	const dimProps = dimensions;
+
+	const rA = dimProps.rotation * (Math.PI / 180)
+	const h = dimProps.height;
+	const w = dimProps.width;
+	const cosA = Math.abs(Math.cos(rA));
+  const sinA = Math.abs(Math.sin(rA));
+
+	const bbH = w * sinA + h * cosA;
+  const bbW = w * cosA + h * sinA;
+
+  let bbX = dimProps.x - (bbW) /2
+  let bbY = dimProps.y - (bbH) /2
+
+	let dim = {
+		x: bbX,
+		y: bbY,
+		width: bbW,
+		height: bbH,
+	}
+
+	return (
+			<>
+				<g style={{ outline: "1px solid green", outlineOffset: "0px" }}>
+					<rect
+						x={dim.x}
+						y={dim.y}
+						width={dim.width}
+						height={dim.height}
+						// transform={transform}
+						stroke='black'
+						stroke-width='1'
+						fill='transparent'
+					/>
+				</g>
+			</>
+		)
+}
+
+
+
+// workaround for outline which doesnt work on firefox
+const BBEllipse: FC<FigureProps> = ({dimensions}) => {
+	const dimProps = dimensions;
+
+	let centerX = dimProps.x;
+	let centerY = dimProps.y;
+	let radiusX = dimProps.width/2;
+	let radiusY = dimProps.height/2;
+	let degrees = dimProps.rotation;
+
+	let radians = degrees * (Math.PI / 180);
+	let radians90 = radians + Math.PI / 2;
+	let ux = radiusX * Math.cos(radians);
+	let uy = radiusX * Math.sin(radians);
+	let vx = radiusY * Math.cos(radians90);
+	let vy = radiusY * Math.sin(radians90);
+
+	let width = Math.sqrt(ux * ux + vx * vx) * 2;
+	let height = Math.sqrt(uy * uy + vy * vy) * 2;
+	let x = centerX - (width / 2);
+	let y = centerY - (height / 2);
+
+	let dim = {
+		x: x,
+		y: y,
+		width: width,
+		height: height,
+	}
+
+	return (
+			<>
+				<g style={{ outline: "1px solid green", outlineOffset: "0px" }}>
+					<rect
+						x={dim.x}
+						y={dim.y}
+						width={dim.width}
+						height={dim.height}
+						// transform={transform}
+						stroke='black'
+						stroke-width='1'
+						fill='transparent'
+					/>
+				</g>
+			</>
+		)
+}
 
 
 
@@ -35,7 +126,7 @@ const Rect: FC<FigureProps> = ({dimensions}) => {
 
 		return (
 			<>
-				<g style={{ outline: "1px solid red", outlineOffset: "0px" }}>
+				<g >
 					<rect
 						x={dim.x}
 						y={dim.y}
@@ -69,7 +160,7 @@ const Ellipse: FC<FigureProps> = ({dimensions}) => {
 
 	return (
 		<>
-			<g style={{ outline: "1px solid red", outlineOffset: "0px" }}>
+			<g>
 					<ellipse
 						cx={dim.x}
 						cy={dim.y}
