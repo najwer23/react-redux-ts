e3e0176b5db3eae2ccb700407628b861610493e5
Mariusz Najwer <najwer23@gmail.com>
2023-05-25 16:27:03 +0200

    Refractoring

--- src/components/stage/Figure.tsx
+++ src/components/stage/Figure.tsx
@@ -1,84 +1,91 @@
 export default function Figure(props: any) {
-	const obj = {
-		id: props.dimensions.id,
-		type: props.dimensions.type,
-		color: props.dimensions.color,
-		rotation: props.dimensions.rotation,
-		x: props.dimensions.x,
-		y: props.dimensions.y,
-		width: props.dimensions.width,
-		height: props.dimensions.height,
-	};
 
-	const transform = `translate(-${obj.width/2}, -${obj.height/2}) rotate(${obj.rotation}, ${obj.x + obj.width/2}, ${obj.y + obj.height/2})`
-
-
-
-	if (props.dimensions.type === "ellips2") {
+	if (props.dimensions.type === "ellipse") {
 		return (
 			<>
-				<g style={{ outline: "1px solid red", outlineOffset: "0px" }}>
-					<ellipse
-						cx={obj.x}
-						cy={obj.y}
-						rx={obj.x}
-						ry={obj.y}
-						width={obj.width}
-						height={obj.height}
-						transform={transform}
-						fill={obj.color}
-					/>
-					<circle
-						cx={obj.x}
-						cy={obj.y}
-						r="4"
-						fill="red"
-					>
-					</circle>
-					<text
-						x={obj.x + 5}
-						y={obj.y}
-						fill="black"
-					>
-						<tspan>{obj.rotation}°</tspan>
-					</text>
-				</g>
+				<Ellipse dimensions={props.dimensions} />
 			</>
 		);
 	}
 
-
-
 	if (props.dimensions.type === "rectangle") {
+		return (
+			<>
+				<Rect dimensions={props.dimensions} />
+			</>
+		);
+	}
+
+	//todo another one
+
+	return <></>
+}
+
+
+function Rect(props: any) {
+	let dim = props.dimensions;
+	const transform = `translate(-${dim.width/2}, -${dim.height/2}) rotate(${dim.rotation}, ${dim.x + dim.width/2}, ${dim.y + dim.height/2})`
+
 		return (
 			<>
 				<g style={{ outline: "1px solid red", outlineOffset: "0px" }}>
 					<rect
-						x={obj.x}
-						y={obj.y}
-						width={obj.width}
-						height={obj.height}
+						x={dim.x}
+						y={dim.y}
+						width={dim.width}
+						height={dim.height}
 						transform={transform}
-						fill={obj.color}
+						fill={dim.color}
 					/>
 					<circle
-						cx={obj.x}
-						cy={obj.y}
+						cx={dim.x}
+						cy={dim.y}
 						r="4"
 						fill="red"
 					>
 					</circle>
 					<text
-						x={obj.x + 5}
-						y={obj.y}
+						x={dim.x + 5}
+						y={dim.y}
 						fill="black"
 					>
-						<tspan>{obj.rotation}°</tspan>
+						<tspan>{dim.rotation}°</tspan>
+					</text>
+				</g>
+			</>
+		)
+}
+
+function Ellipse(props: any) {
+	let dim = props.dimensions;
+	const transform = `rotate(${dim.rotation}, ${dim.x}, ${dim.y})`
+
+	return (
+		<>
+			<g style={{ outline: "1px solid red", outlineOffset: "0px" }}>
+					<ellipse
+						cx={dim.x}
+						cy={dim.y}
+						rx={dim.width/2}
+						ry={dim.height/2}
+						transform={transform}
+						fill={dim.color}
+					/>
+					<circle
+						cx={dim.x}
+						cy={dim.y}
+						r="4"
+						fill="red"
+					>
+					</circle>
+					<text
+						x={dim.x + 5}
+						y={dim.y}
+						fill="black"
+					>
+						<tspan>{dim.rotation}°</tspan>
 					</text>
 				</g>
 		</>
-		);
-	}
-
-	return <></>
+	)
 }
